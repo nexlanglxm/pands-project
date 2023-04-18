7c655175778abd6ae0e37e78418055cef78a89f4
Neil Anglim <G00425634@atu.ie>
2023-04-18 13:06:11 +0100

    Playing around with the pandas module

--- analysis.py
+++ analysis.py
@@ -1,1 +1,11 @@
 #pandas will be used to output the summaries into a single text file
+import pandas as pd
+
+filename = 'iris.data'
+df = pd.read_csv(filename)
+
+df.to_frame(iris.txt)
+
+pd.DataFrame(filename)
+
+print(df)
