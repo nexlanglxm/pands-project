f23f4bc5a9c7b3ec4ff86aa70df2a2a9ceffc810
Neil Anglim <G00425634@atu.ie>
2023-04-20 12:59:08 +0100

    Working on columns with pandas

--- analysis.py
+++ analysis.py
@@ -7,10 +7,38 @@ import matplotlib.pyplot as plt
 '''firstly, the data will be called in and displayed using pandas,
  and then output to a text file'''
 
-#filename = 'iris.data'
-Iris = pd.read_csv("C:/Users//neilj/pands/pands-project/iris.data")
-Iris.info()
-Iris
+'''column_subset = [
+    "sepal length",
+    "sepal width",
+    "petal length",
+    "petal width",
+    "flower",
+]'''
+Iris = pd.read_csv(
+    "iris.data",
+    #usecols=column_subset,
+    nrows=149
+    )
+
+#providing values for the new row to be input as a dictionary
+new_row = ("Sepal length",
+    "Sepal width",
+    "Petal length",
+    "Petal width",
+    "Flower",
+    )
+Iris.loc[0] = new_row
+'''
+Iris.columns = ["Sepal length",
+    "Sepal width",
+    "Petal length",
+    "Petal width",
+    "Flower",
+    ]'''
+print(Iris)
+#Iris.head()
+#Iris.info()
+#Iris.to_frame(iris.txt)
 #df.to_frame(iris.txt)
 
 
